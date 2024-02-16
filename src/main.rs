b54592c815ec1ab4f26bc003ba4d57d1d84a911e
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2024-02-16 20:53:01 +0100

    feat: 🌈 Port to axum
    hyper on its own is a bit too low level even for such a simple case, now that the v1
    removed important features such as
    graceful shutdown.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
-use bytes::Bytes;
-use http_body_util::Empty;
-use hyper::body::Incoming;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::http::Uri;
+use axum::response::Redirect;
+use axum::Router;
 use std::env;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::TcpListener;
-use tokio::pin;
+use tokio::signal;
+use tower::ServiceBuilder;
+use tower_http::trace;
+use tower_http::{timeout::TimeoutLayer, trace::TraceLayer, ServiceBuilderExt};
+use tracing::Level;
 
-async fn redirect(
-    redirect_url: Arc<String>,
-    req: Request<Incoming>,
-) -> Result<Response<Empty<Bytes>>, hyper::Error> {
-    let path_and_query = req.uri().path_and_query().unwrap();
-    println!("Redirecting to {}{}", &redirect_url, path_and_query);
-    let full_url = format!("{}{}", &redirect_url, path_and_query);
-    Ok(Response::builder()
-        .status(301)
-        .header("location", full_url)
-        .body(Empty::new())
-        .unwrap())
+#[derive(Clone, Debug)]
+pub struct HttpServerState {
+    pub redirect_url: Arc<String>,
+}
+
+async fn redirect(State(state): State<HttpServerState>, uri: Uri) -> Result<Redirect, StatusCode> {
+    let path_and_query = uri.path_and_query().unwrap().as_str();
+    let redirect_url = state.redirect_url;
+    //println!("Redirecting to {}{}", redirect_url, path_and_query);
+    //let full_url = format!("{}{}", redirect_url, path_and_query);
+    let mut full_url = String::with_capacity(path_and_query.len() + redirect_url.len());
+    full_url.push_str(&redirect_url);
+    full_url.push_str(path_and_query);
+    Ok(Redirect::permanent(&full_url))
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -38,51 +42,56 @@ async fn main() {
         .expect("Failed to bind to address");
     println!("Listening on http://{}", address);
 
-    // Use a 5 second timeout for incoming connections to the server.
-    // If a request is in progress when the 5 second timeout elapses,
-    // use a 2 second timeout for processing the final request and graceful shutdown.
-    let connection_timeouts = vec![Duration::from_secs(5), Duration::from_secs(2)];
+    // Initialize tracing
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
 
-    loop {
-        let redirect_url_task = redirect_url.clone();
-        let (tcp, remote_address) = listener
-            .accept()
+    // Middleware creation
+    let middleware = ServiceBuilder::new()
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        )
+        .layer(TimeoutLayer::new(Duration::from_secs(10)))
+        .compression()
+        .into_inner();
+
+    let state = HttpServerState { redirect_url };
+
+    let app = Router::new()
+        .fallback(redirect)
+        .layer(middleware)
+        .with_state(state);
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .expect("Failed to start server");
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
             .await
-            .expect("Failed to accept connection");
-        let io = TokioIo::new(tcp);
+            .expect("failed to install Ctrl+C handler");
+    };
 
-        println!("Accepted connection from: {:?}", remote_address);
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
 
-        let connection_timeouts_clone = connection_timeouts.clone();
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
 
-        tokio::task::spawn(async move {
-            let conn = http1::Builder::new().serve_connection(
-                io,
-                service_fn(move |req| redirect(redirect_url_task.clone(), req)),
-            );
-            pin!(conn);
-
-            // Iterate the timeouts.  Use tokio::select! to wait on the
-            // result of polling the connection itself,
-            // and also on tokio::time::sleep for the current timeout duration.
-            for sleep_duration in connection_timeouts_clone.iter() {
-                tokio::select! {
-                    res = conn.as_mut() => {
-                        // Polling the connection returned a result.
-                        // In this case print either the successful or error result for the connection
-                        // and break out of the loop.
-                        if let Err(e) = res {
-                            println!("error serving connection: {:?}", e);
-                        }
-                        break;
-                    }
-                    _ = tokio::time::sleep(*sleep_duration) => {
-                        // tokio::time::sleep returned a result.
-                        // Call graceful_shutdown on the connection and continue the loop.
-                        conn.as_mut().graceful_shutdown();
-                    }
-                }
-            }
-        });
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
     }
 }
